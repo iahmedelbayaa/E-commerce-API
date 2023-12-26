9ca8519a8a7b853ef18cc922648ddb291cee0bd8
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-26 07:50:00 +0200

    docs- create-file-structure

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+
+
+
+
+const app = express();
+
+
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
