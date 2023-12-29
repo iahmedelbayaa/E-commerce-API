93343799e662357a3ada861fea89283b8951c047
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-29 09:55:44 +0200

    Fix user & login router

--- src/routes/login-route.ts
+++ src/routes/login-route.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import * as signupCtrl from '../controller/login-controller';
-import signupSchema from '../validations/login-schema';
+import signInSchema from '../validations/login-schema';
 import validate from '../middleware/validator';
 import * as authentication from '../middleware/authentication'
 
 const router = express.Router();
 
-router.post('/login',validate(signupSchema), signupCtrl.login);
+router.post('/login', validate(signInSchema), signupCtrl.login);
 
 
 export default router;
--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,29 +1,17 @@
 import express from 'express';
 import * as userCtrl from '../controller/user-controller';
+import { Roles } from '../util/roles';
+import { authorizeByRole } from '../middleware/authorization';
+
+
 const router = express.Router();
-import * as authorization from '../middleware/authorization';
-import {Roles} from '../util/roles';
 
 
-router.get(
-  '/getAllUser',
-  authorization.authorizeByRole([Roles.ADMIN]),
-  userCtrl.getAll
-);
-router.get(
-  '/user/:id',
-  authorization.authorizeByRole(Roles.ALL),
-  userCtrl.getById
-);
-router.put(
-  '/update/:id',
-  authorization.authorizeByRole(Roles.ALL),
-  userCtrl.update
-);
-router.delete(
-  '/delete/:id',
-  authorization.authorizeByRole([Roles.ADMIN]),
-  userCtrl.remove
-);
+router.get('/getAllUser', authorizeByRole([Roles.ADMIN]), userCtrl.getAll);
+router.get('searchAll', authorizeByRole(Roles.ALL), userCtrl.searchAll);
+router.get('/user/:id', authorizeByRole(Roles.ALL), userCtrl.getById);
+router.get('/user/:id/roles', authorizeByRole(Roles.ADMIN), userCtrl.getRole);
+router.put('/update/:id', authorizeByRole(Roles.ALL), userCtrl.update);
+router.delete('/delete/:id', authorizeByRole([Roles.ADMIN]), userCtrl.remove);
 
 export default router;
