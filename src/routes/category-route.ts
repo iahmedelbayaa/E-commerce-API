66ce9c5d3241937c279cf8e55b511a74f1a8f112
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-29 10:44:52 +0200

    fix category router

--- src/routes/category-route.ts
+++ src/routes/category-route.ts
@@ -1,39 +1,39 @@
 import express from 'express';
 const router = express.Router();
 import * as categoryController from '../controller/category-controller';
-import * as authentication from '../middleware/authentication';
-import * as authorization from '../middleware/authorization';
+import {verifyToken} from '../middleware/authentication';
+import { authorizeByRole } from '../middleware/authorization';
 import  {categorySchema}  from '../validations/categorySchema';
 import validator from '../middleware/validator';
 import {Roles} from '../util/roles';
 
-router.use(authentication.verifyToken);
+router.use(verifyToken);
 
 
 router.get(
   '/',
-  authorization.authorizeByRole([Roles.ADMIN]),
+  authorizeByRole([Roles.ADMIN]),
   categoryController.getAll
 );
 router.get(
   '/searchAll',
-  authorization.authorizeByRole(Roles.ALL),
+  authorizeByRole(Roles.ALL),
   categoryController.searchOne
 );
 router.get(
   '/:id',
-  authorization.authorizeByRole([Roles.ADMIN, Roles.SELLER]),
+  authorizeByRole([Roles.ADMIN, Roles.SELLER]),
   categoryController.getById
 );
 router.get(
   '/:id/products',
-  authorization.authorizeByRole(Roles.ALL),
+  authorizeByRole(Roles.ALL),
   categoryController.getCategoryProducts
 );
 
 router.post(
   '/',
-  authorization.authorizeByRole([Roles.ADMIN, Roles.SELLER]),
+  authorizeByRole([Roles.ADMIN, Roles.SELLER]),
   validator(categorySchema),
   categoryController.save
 );
