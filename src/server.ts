67e10b68c8088d5c96dbe883c4297dd456907a58
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-30 17:20:36 +0200

    docs server with router

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,12 @@ import userRouter from './routes/user-router'
 import roleRouter from './routes/role-route'
 import signupRouter from './routes/signup-route'
 import loginRouter from './routes/login-route'
+import productRoute from './routes/product-route';
+import userProductRoute from './routes/user-product-route';
+import cartRoute from './routes/cart-route';
+import cartItemRoute from './routes/cart-item-route';
+import orderRoute from './routes/order-route';
+import categoryRoute from './routes/category-route';
 
 
 
@@ -26,7 +32,15 @@ app.use(express.json());
 app.use('/api/v1', userRouter);
 app.use('/api/v1', roleRouter);
 app.use('/api/v1', signupRouter);
-app.use('/api/v1' , loginRouter);
+app.use('/api/v1', loginRouter);
+app.use('/api/v1', productRoute);
+app.use('/api/v1', userProductRoute);
+app.use('/api/v1', cartRoute);
+app.use('/api/v1', cartItemRoute);
+app.use('/api/v1', orderRoute);
+app.use('/api/v1', categoryRoute);
+
+
 
 
 
