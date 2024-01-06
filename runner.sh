629b2f8b40139b5b8fdf9ad8e63bc4ad27f02131
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-06 19:52:48 +0200

    add runner.sh

---
+++ runner.sh
@@ -0,0 +1,10 @@
+docker run -d --name runner-1 \
+    -e GITHUB_USERNAME=iahmedelbayaa \
+    -e GITHUB_REPOSITORY=github-actions \
+    -e GITHUB_PERSONAL_ACCESS_TOKEN=\
+    -e RUNNER_NAME=githubAction-Runner-1 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /usr/bin/docker:/usr/bin/docker \
+    omarhosny102/github-runner
+
+    # for create runner for ES2 aws machine 
