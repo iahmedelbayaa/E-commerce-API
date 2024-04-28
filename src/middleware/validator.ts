7cda978ca0af99b8f5c695e1d5c8ba5b7f914782
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-04-28 19:11:05 +0300

    Fix bugs

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -4,9 +4,9 @@ import { Response , Request , NextFunction } from 'express';
 const validate = (schema: any) => async (req: Request, res: Response, next : NextFunction) => {
     try {
         const body = req.body;
-        const value = await schema.validateAsync(body, { abortEarly: false });
+        await schema.validateAsync(body, { abortEarly: false });
         next();
-    } catch (error) {
+    } catch (error: any) {
         const messages = error.details.map((errorDetails: any) => errorDetails.message);
         next(ApiError.badRequest(messages));
     }
