3628bd97a437e18ca35d3357d15406e5d84c4752
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-04-28 18:57:27 +0300

    Docs user ctrl

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -6,17 +6,17 @@ export const getAll = async (req: Request, res: Response) => {
   try {
     const users = await userService.getAll();
     res.status(StatusCode.OK).json(users);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
 
 export const getById = async (req: Request, res: Response) => {
   try {
-    const id = parseInt(req.params.id);
+    const id = +(req.params.id);
     const user = await userService.getById(id);
     res.status(StatusCode.OK).json(user);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -25,7 +25,7 @@ export const getByEmail = async (req: Request, res: Response) => {
   try {
     const user = await userService.getByEmail(req.params.email);
     res.status(StatusCode.OK).json(user);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -34,18 +34,17 @@ export const searchAll = async (req: Request, res: Response) => {
   try {
     const users = await userService.searchAll(req.body);
     res.status(StatusCode.OK).json(users);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
 
 export const getRole = async (req: Request, res: Response) => {
   try {
-    const id = parseInt(req.params.id);
-
+    const id = +(req.params.id);
     const user = await userService.getRole(id);
     res.status(StatusCode.OK).json(user);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -54,29 +53,28 @@ export const save = async (req: Request, res: Response) => {
   try {
     await userService.save(req.body);
     res.status(StatusCode.CREATED).json({ message: 'saved successfully' });
-  } catch (error) {
-    console.error(error);
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
 
 export const update = async (req: Request, res: Response) => {
   try {
-    const id = parseInt(req.params.id);
+    const id = +(req.params.id);
 
-    const user = await userService.update({id, ...req.body});
+    await userService.update({id, ...req.body});
     res.status(StatusCode.OK).json({ message: 'Updating successfully' });
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
 
 export const remove = async (req: Request, res: Response) => {
   try {
-    const id = parseInt(req.params.id);
-    const user = await userService.remove(id);
+    const id = +(req.params.id);
+    await userService.remove(id);
     res.status(StatusCode.OK).json({ message: 'Deleted successfully' });
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
