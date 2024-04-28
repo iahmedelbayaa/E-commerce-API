9a4d3e633489555080d5e7449b294a80cd42ee07
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-04-28 18:46:48 +0300

    Fix category controller

--- src/controller/category-controller.ts
+++ src/controller/category-controller.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express';
 import * as categoryService from '../services/category-service';
-import * as productService from '../services/user-service';
 import { StatusCode } from '../util/status-code';
 import { tables } from '../util/tables';
 
-const Product = tables.product;
 const Category = tables.category;
 
 
@@ -12,7 +10,7 @@ export const getAll = async (req: Request, res: Response) => {
   try {
     const users = await categoryService.getAll();
     res.status(StatusCode.OK).json(users);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -21,15 +19,16 @@ export const searchOne = async (req: Request, res: Response) => {
   try {
     const users = await categoryService.searchOne(req.body);
     res.status(StatusCode.OK).json(users);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
 export const getById = async (req: Request, res: Response) => {
   try {
-    const user = await categoryService.getById(req.params.id);
+    const categoryId = +(req.params.id)
+    const user = await categoryService.getById(categoryId);
     res.status(StatusCode.OK).json(user);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -37,9 +36,10 @@ export const getById = async (req: Request, res: Response) => {
 
 export const getCategoryProducts = async (req: Request, res: Response) => {
   try {
-    const product = await categoryService.getProducts(req.params.id);
+    const categoryId = +(req.params.id)
+    const product = await categoryService.getProducts(categoryId);
     res.status(StatusCode.OK).json(product);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -50,7 +50,7 @@ export const searchAll = async (req: Request, res: Response) => {
   try {
     const users = await categoryService.searchAll(req.body);
     res.status(StatusCode.OK).json(users);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -60,7 +60,7 @@ export const save = async (req: Request, res: Response) => {
   try {
     const category = await Category.create(req.body);
     res.status(StatusCode.CREATED).json(category);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -70,7 +70,7 @@ export const update = async (req: Request, res: Response) => {
   try {
     const category = await categoryService.update(req.body);
     res.status(StatusCode.CREATED).json(category);
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
@@ -78,9 +78,10 @@ export const update = async (req: Request, res: Response) => {
 
 export const remove = async (req: Request, res: Response) => {
   try {
-    const product = await categoryService.deleteProduct(req.params.id);
+    const categoryId = +(req.params.id)
+    await categoryService.deleteProduct(categoryId);
     res.status(StatusCode.CREATED).json({ message: 'removed successfully' });
-  } catch (error) {
+  } catch (error: any) {
     res.status(StatusCode.BAD_REQUEST).json({ message: error.message });
   }
 };
